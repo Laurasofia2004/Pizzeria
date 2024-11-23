48d130d9b0e15abe20d5c9b7c5d8540b0787355c
Juanes2192 <juanestebanperdomoleiva@gmail.com>
2024-11-23 16:50:13 -0500

    CRUD de pizza

---
+++ pizzeria/resources/views/pizza/new.blade.php
@@ -0,0 +1,78 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Crear Pizzas') }}
+        </h2>
+    </x-slot>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <form method="POST" action="{{ route('pizzas.store') }}">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="id" class="form-label">Id</label>
+                            <input type="text" class="form-control" id="id" name="id" disabled="disabled">
+                            <div id="idHelp" class="form-text">Pizza id</div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Nombre de Pizza</label>
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Nombre de Pizza">
+                        </div>
+                        <div class="mt-3">
+                            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Save</button>
+                            <a href="{{ route('pizzas.index') }}" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded ml-2">Cancel</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
+
+<!-- Alertas de creacion -->
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<script>
+    $(document).ready(function() {
+        $('form').on('submit', function(e) {
+            e.preventDefault();
+            // Variables de los campos
+            var name = $('#name').val();
+            // Validaciones
+            if (!name) {
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: "top-end",
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                        toast.onmouseenter = Swal.stopTimer;
+                        toast.onmouseleave = Swal.resumeTimer;
+                    }
+                });
+                $('#name').focus();
+                Toast.fire({
+                    icon: "error",
+                    title: "Completa todos los campos."
+                });
+                return;
+            }
+
+            Swal.fire({
+                icon: 'success',
+                title: 'Pizza creada con éxito',
+                showConfirmButton: false,
+                timer: 1500
+            }).then(function() {
+                // Si todo es correcto, proceder con el envío del formulario
+                $('form').unbind('submit').submit();
+            });
+
+        });
+    });
+</script>
